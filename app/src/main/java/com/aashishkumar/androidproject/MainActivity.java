09d918a826595edfe9a9e08110c1a7e2c0f01ed3
unknown <robertbohlman@hotmail.com>
2018-10-30 15:19:18 -0700

    Added initial home screen implementation

--- app/src/main/java/com/aashishkumar/androidproject/MainActivity.java
+++ app/src/main/java/com/aashishkumar/androidproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.aashishkumar.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //This is currently to load the home activity for testing purposes.
+        //Remove once login is implemented.
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
